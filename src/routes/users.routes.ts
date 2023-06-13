36f5d800bc8c6f88b36f836f1fbf75b48f575a80
To Truong Chinh <totruongchinh@gmail.com>
2023-06-14 00:00:24 +0700

    use checkSchema to validate body register

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import { validate } from '~/utils/validation'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
+
+/**
+ * Decription. Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: {name: string, email: string, password: string, confirm_password: string, day_of_birth: ISO8601}
+ */
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
